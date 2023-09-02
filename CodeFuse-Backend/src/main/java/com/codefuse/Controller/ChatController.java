4d7faf047fb48f6fbd649af825b8409484a85e34
Aniket Sengar <75060938+Anklet9@users.noreply.github.com>
2023-09-02 22:42:09 +0530

    Done and Dusted

--- CodeFuse-Backend/src/main/java/com/codefuse/Controller/ChatController.java
+++ CodeFuse-Backend/src/main/java/com/codefuse/Controller/ChatController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.codefuse.DTO.ChatResponse;
 import com.codefuse.config.NoResponseException;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ChatController {
     
     @Qualifier("openaiRestTemplate")
