f95684b47f33e61183c98adb4b54395030b430de
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-09-02 19:20:23 +0530

    chat box code

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChatBox } from './Components/ChatBox';
 
 function App() {
   return (
     <div className="App">
-
+      <ChatBox />
     </div>
   );
 }
