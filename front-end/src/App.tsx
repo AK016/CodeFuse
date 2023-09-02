05307b6610a43b11cf073eb5f80b2f81a1132e05
swati-anchatageri <swatianchatageri129@gmail.com>
2023-09-02 19:58:23 +0530

    homepage

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer.';
+import MainRoutes from './Routes/MainRoute';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar/>
+      <MainRoutes/>
+      <Footer/>
     </div>
   );
 }
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
