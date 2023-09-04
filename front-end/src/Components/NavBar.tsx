c9c46e1627093564807c6b1ea8855c9eb213912b
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-09-04 20:19:04 +0530

    logo change

--- front-end/src/Components/NavBar.tsx
+++ front-end/src/Components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../images/codefuse.png"
+import logo from "../images/codefuse-logo.png"
 import { AiOutlineBell } from 'react-icons/ai';
 const NavBar = () => {
   const navigation = [
@@ -56,7 +56,7 @@ const NavBar = () => {
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 export default NavBar;
